a8e0bc3ac11b3dd4543167d6e3cb6f9121245d73
wilder <marionfontaine904@gmail.com>
2017-04-27 11:51:31 +0200

    tableLayout

--- app/src/main/java/com/example/wilder/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wilder/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.wilder.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tex
+
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
 }
