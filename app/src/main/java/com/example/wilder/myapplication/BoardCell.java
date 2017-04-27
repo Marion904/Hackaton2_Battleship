5784b0d9ed427dc913173caacfaa74f2bc95ed57
mathieu regourd <mathieu.rgrd@gmail.com>
2017-04-27 16:11:04 +0200

    ShipClass and stuff

---
+++ app/src/main/java/com/example/wilder/myapplication/BoardCell.java
@@ -0,0 +1,107 @@
+package com.example.wilder.myapplication;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by mathieu on 27/04/17.
+ */
+
+
+public class BoardCell implements Parcelable {
+
+    protected int mPosX;
+    protected int mPosY;
+    protected boolean mHitted;
+    protected boolean mOccupied;
+    protected Ship mShipOver;
+
+    public BoardCell(int x, int y) {
+        mPosX = x;
+        mPosY = y;
+        mHitted = false; //Toucher
+        mOccupied = false;// Placer sur la case
+        mShipOver = null; //Couler
+    }
+
+    private BoardCell(Parcel in) {
+        mPosX = in.readInt();
+        mPosY = in.readInt();
+        //...and then convert it back into a boolean
+        mHitted = (in.readByte() == 1);
+        mOccupied = (in.readByte() == 1);
+        mShipOver = in.readParcelable(null);
+    }
+
+    public int getPosX() {
+        return mPosX;
+    }
+
+    public void setPosX(int mPosX) {
+        this.mPosX = mPosX;
+    }
+
+    public int getPosY() {
+        return mPosY;
+    }
+
+    public void setPosY(int mPosY) {
+        this.mPosY = mPosY;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mPosX);
+        dest.writeInt(mPosY);
+        //To write a boolean in a parcel you have to convert it into a byte...
+        dest.writeByte((byte) (mHitted ? 1 : 0));
+        dest.writeByte((byte) (mOccupied ? 1 : 0));
+        dest.writeParcelable((Parcelable) mShipOver, 0);
+    }
+
+    public static final Parcelable.Creator<BoardCell> CREATOR
+            = new Parcelable.Creator<BoardCell>() {
+
+        public BoardCell createFromParcel (Parcel in) {
+            return new BoardCell(in);
+        }
+
+        public BoardCell[] newArray(int size) {
+            return new BoardCell[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public boolean isHitted() {
+
+        return mHitted;
+    }
+
+    public boolean isOccupied() {
+
+        return mOccupied;
+    }
+
+    public void hit() {
+
+        mHitted = true;
+    }
+
+    public Ship getShipOver() {
+        return mShipOver;
+    }
+
+    public void setShipOver(Ship mShipOver) {
+        this.mShipOver = mShipOver;
+        mOccupied = true;
+    }
+
+    public void free() {
+        mOccupied = false;
+    }
+
+}
--- app/src/main/java/com/example/wilder/myapplication/GameActivity.java
+++ app/src/main/java/com/example/wilder/myapplication/GameActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
@@ -17,9 +18,14 @@ public class GameActivity extends AppCompatActivity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
 
+
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
+
+                ImageView imageView = (ImageView) v ;
+                imageView.setImageResource(R.drawable.explodepng);
+
                 Toast.makeText(GameActivity.this, "" + position,
                         Toast.LENGTH_SHORT).show();
             }
